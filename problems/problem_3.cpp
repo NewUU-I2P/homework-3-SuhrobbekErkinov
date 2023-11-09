c6d2e43111ecb12e053339931b2aca048f6beb1f
SuhrobbekErkinov <s.erkinov@newuu.uz>
2023-11-09 11:56:10 +0500

    7th

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -3,7 +3,7 @@
 std::string problemSolution3(float height, char S) {
     // write your code here
     std::string result;
-    if (S = 'M' && S = 'm') {
+    if (S = 'M' || S = 'm') {
         if (height < 1.70) {
             result = "Short";
         } else if (height >= 1.70 && height < 1.85) {
@@ -11,7 +11,7 @@ std::string problemSolution3(float height, char S) {
         } else if (height >= 1.85) {
             result = "Tall";
         }
-    } else if (S = 'F' && S = 'f') {
+    } else if (S = 'F' || S = 'f') {
         if (height < 1.60) {
             result = "Short";
         } else if (height >= 1.60 && height < 1.75) {
