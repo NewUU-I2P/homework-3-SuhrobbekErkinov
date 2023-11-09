9fe6e062050d42dfe68f753503f6206ea34d84a4
SuhrobbekErkinov <s.erkinov@newuu.uz>
2023-11-09 11:55:07 +0500

    6th

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,9 +2,8 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
-    string result;
-    std::cin >> S >> height;
-    if (S = 'M') {
+    std::string result;
+    if (S = 'M' && S = 'm') {
         if (height < 1.70) {
             result = "Short";
         } else if (height >= 1.70 && height < 1.85) {
@@ -12,7 +11,7 @@ std::string problemSolution3(float height, char S) {
         } else if (height >= 1.85) {
             result = "Tall";
         }
-    } else if (S = 'F') {
+    } else if (S = 'F' && S = 'f') {
         if (height < 1.60) {
             result = "Short";
         } else if (height >= 1.60 && height < 1.75) {
@@ -22,5 +21,6 @@ std::string problemSolution3(float height, char S) {
         }
     }
     // use return to return your result
+    return result;
     // make use of control flow statements
 }
