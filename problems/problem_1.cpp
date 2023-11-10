e512e7e441aad61c04875aafa34e137627848dba
Suhrobbek <147584562+SuhrobbekErkinov@users.noreply.github.com>
2023-11-10 13:02:01 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -3,13 +3,13 @@ float problemSolution1(float consumed_water) {
     // write your code here;
     if (consumed_water <= 30) {
         cost = 13 + consumed_water * 0.4;
-    } else if (consumed_water <= 50) {
+    } else if (consumed_water <= 50 && consumed_water > 30) {
         cost = 13 + 30 * 0.4 + (consumed_water - 30) * 0.12;
-    } else if (consumed_water <= 60) {
+    } else if (consumed_water <= 60 && consumed_water > 50) {
         cost = 13 + 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
-    } else {
+    } else if (consumed_water > 60) {
         cost = 13 + 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.5;
-    }
+    } 
 
     return cost;
 }
